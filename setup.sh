e7f514f135ef7d620d2268bea7dac0c38d2788ce
danielshougam <daniel.shougam@gmail.com>
2018-06-25 17:22:36 +1000

    vim and vs code working

--- setup.sh
+++ setup.sh
@@ -2,7 +2,13 @@
 
 # install everything
 apt-get update
-apt-get install build-essential i3 vim git python python3 python-pip python3-pip neovim curl
+apt-get install software-properties-common
+apt-add-repository ppa:neovim-ppa/stable
+apt-get update
+apt-get install build-essential i3 vim python python3 python-pip python3-pip \
+    curl neovim cmake python-dev python3-dev
+pip install setuptools
+pip3 install setuptools
 pip3 install neovim
 pip install neovim
 
@@ -11,3 +17,13 @@ mkdir -p ../.config/nvim
 cp ./init.vim ../.config/nvim/init.vim
 curl -fLo ../.local/share/nvim/site/autoload/plug.vim --create-dirs \
 	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+nvim +PlugInstall +qall
+
+sudo python3 ~/.local/share/nvim/plugged/YouCompleteMe/install.py --clang-completer
+
+# vs code
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list
+apt-get update
+apt-get install code
